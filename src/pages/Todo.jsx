d8afa077b4a29afc0957fb5c795063fa1676b4fb
unknown <firdhair30@gmail.com>
2023-04-01 15:01:14 +0700

    initialization and setting up technical test project

---
+++ src/pages/Todo.jsx
@@ -0,0 +1,10 @@
+import { useState, useEffect } from 'react'; 
+import styles from "./Todo.module.scss"
+
+const Todo = () => {
+    return(
+        <p className={styles.todo}>ini todo</p>
+    )
+}
+
+export default Todo
