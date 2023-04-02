aeb278d8ace662ccecb0867087c8a3d22bb2506e
unknown <firdhair30@gmail.com>
2023-04-02 13:44:02 +0700

    Created finish/mark item functionality

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,32 +1,72 @@
+import { useState } from 'react'; 
 import styles from "./List.module.scss"
 import { ReactComponent as CheckCircle } from "../media/icons/CheckCircle.svg"
 import { ReactComponent as MinusCircle } from "../media/icons/MinusCircle.svg"
 
 const List = ({tasks, setTasks}) => {
-  //console.log("list tasks", tasks)
-  const deleteTask = (id) => {
-    console.log("id", id)
+  const [color, setColor] = useState("#F5F5F4")
+  const [opacity, setOpacity] = useState(1)
 
+  const finishTask = (id) => {
+    setColor('#059669')
+    setOpacity(0.5)
+
+    tasks.map((task, i) => {
+      if(task['id'] === id) {
+        const newTask = [...tasks]
+        newTask[i] = {
+          id: id,
+          name: newTask[i]['name'],
+          status: true
+        }
+        setTasks(newTask)
+      }
+    })
+  }
+
+  const deleteTask = (id) => {
     const taskDelete = tasks 
     const updateTask = taskDelete.filter((task) => task.id !== id)
 
     setTasks(updateTask)
   }
 
+  console.log("tasks", tasks)
   return (
+    <>
         <ul>
           {tasks.map((task) => (
-           <div className={styles.task}>
-              <li key={task['id']} className={styles.task__info}>
-                <div className={styles.task__info__checklist}>
-                  <CheckCircle className={styles.task__info__check}/>
-                  <p> {task['name']} </p>
-                </div>
-                <MinusCircle className={styles.task__info__remove} onClick={(e) => deleteTask(task['id'])}/>
-              </li>
-           </div>
+            <>
+              {!task['status'] ? 
+              <div className={styles.task}>
+                  <li key={task['id']} className={styles.task__info}>
+                    <div className={styles.task__info__checklist}>
+                      <CheckCircle className={styles.task__info__check} onClick={(e) => finishTask(task['id'])} style={{fill: "#f5f5f4"}}/>
+                      <p> {task['name']} {task['status']}</p>
+                    </div>
+                    <MinusCircle className={styles.task__info__remove} onClick={(e) => deleteTask(task['id'])}/>
+                  </li>
+              </div>
+              : <></>}
+            </>
+          ))}
+          {tasks.map((task) => (
+            <>
+              {task['status'] ?      
+              <div className={styles.task}>
+                  <li key={task['id']} className={styles.task__info} style={{opacity: task['status'] ? 0.5 : 1}}>
+                    <div className={styles.task__info__checklist}>
+                      <CheckCircle className={styles.task__info__check} onClick={(e) => finishTask(task['id'])} style={{fill: task['status'] ? "#059669" : "#f5f5f4"}}/>
+                      <p> {task['name']} {task['status']}</p>
+                    </div>
+                    <MinusCircle className={styles.task__info__remove} onClick={(e) => deleteTask(task['id'])}/>
+                  </li>
+              </div>
+              : <></>}
+            </>
           ))}
         </ul>
+    </>
   );
 };
 export default List;
--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,6 +1,7 @@
 // import necessary dependencies //
 import { useState } from 'react'; 
 import uniqid from "uniqid"
+import Modal from 'react-modal';
 // import styling //
 import styles from "./Todo.module.scss"
 // import necessary components //
@@ -12,6 +13,7 @@ const Todo = () => {
     const [tasks, setTasks] = useState([])
     const [isAddNew, setIsAddNew] = useState(false)
     const [isMoreThan100, setIsMoreThan100] = useState(false)
+    const [isOpen, setIsOpen] = useState(false);
     //const [isLessThan0, setIsLessThan0] = useState(false)
 
     const onSubmitTask = (e) => {
@@ -35,7 +37,18 @@ const Todo = () => {
         });
     }
 
-    console.log("isMoreThan100", isMoreThan100)
+    const removeAllTasks = () => {
+        setTasks([])
+    }
+
+    // const openModal = () => {
+    //     setIsOpen(true)
+    // }
+
+    // const closeModal = () => {
+    //     setIsOpen(false)
+    // }
+
     return(
         <div className={styles.todo_wrapper}>  
             <div className={styles.todo_container}>
@@ -46,24 +59,31 @@ const Todo = () => {
                             // <Button addNew={isAddNew} setIsAddNew={setIsAddNew} name={"Add New"}/>: 
                             <button onClick={() => setIsAddNew(!isAddNew)}>Add New</button>:
                         <></> }
-                        <p>Clear</p>
+                        <p onClick={() => removeAllTasks()}>Clear</p>
                     </div>
                 </div>
+                {/* <Modal
+                    isOpen={isOpen}
+                    onRequestClose={closeModal}
+                    contentLabel="Example Modal"
+                >
+                    <h2>Modal Title</h2>
+                    <p>Modal content goes here.</p>
+                    <button onClick={closeModal}>Close Modal</button>
+                </Modal> */}
                 <div className={styles.todo_addNewTask}>
                     {isAddNew ? 
-                    // isMoreThan100
                     <>
-                    <form onSubmit={onSubmitTask}>
-                        <textarea type="text" className='' onChange={(e) => handleChange(e)} placeholder="Add new to-do title..." rows="2"/>
-                        <div className={styles.todo_addNewTask__options}>
-                            <p onClick={() => setIsAddNew(!isAddNew)}>Cancel</p>
-                            <button type='submit'>Create</button>
-                        </div> 
-                    </form>
-                    {isMoreThan100 === true ? 
-                    <p className={styles.warning}>Title must be shorter than equal to 100 characters.</p> :
-                    <></>
-                    }
+                        <form onSubmit={onSubmitTask}>
+                            <textarea type="text" className='' onChange={(e) => handleChange(e)} placeholder="Add new to-do title..." rows="2"/>
+                            <div className={styles.todo_addNewTask__options}>
+                                <p onClick={() => setIsAddNew(!isAddNew)}>Cancel</p>
+                                <button type='submit'>Create</button>
+                            </div> 
+                        </form>
+                        {isMoreThan100 === true ? 
+                            <p className={styles.warning}>Title must be shorter than equal to 100 characters.</p> : <></>
+                        }
                     </>
                     : <></>
                     }
